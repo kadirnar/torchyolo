6bf00df1cd10f64b2967e59977af7cd396079cfb
kadirnar <kadir.nar@hotmail.com>
2023-01-01 14:40:49 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,54 @@
+import io
+import os
+import re
+
+import setuptools
+
+
+def get_long_description():
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    with io.open(os.path.join(base_dir, "README.md"), encoding="utf-8") as f:
+        return f.read()
+
+
+def get_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+def get_version():
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    version_file = os.path.join(current_dir, "torchyolo", "__init__.py")
+    with io.open(version_file, encoding="utf-8") as f:
+        return re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
+
+
+setuptools.setup(
+    name="torchyolo",
+    version=get_version(),
+    author="kadirnar",
+    license="GPL-3.0",
+    description="PyTorch implementation of YOLOv5, YOLOv6, YOLOv7, YOLOX",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kadirnar/torchyolo",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=get_requirements(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Education",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    keywords="machine-learning, deep-learning, pytorch, vision, yolov6,yolox, object-detection, yolov7, detector, yolov5"
+)
