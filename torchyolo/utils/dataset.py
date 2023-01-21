54711fac06ec4c0e1213c68240ae2dfb041f03d5
kadirnar <kadir.nar@hotmail.com>
2023-01-21 12:19:56 +0300

    The save_path codes have been updated.

--- torchyolo/utils/dataset.py
+++ torchyolo/utils/dataset.py
@@ -78,10 +78,6 @@ def create_video_writer(video_path, output_path, fps=None) -> cv2.VideoWriter:
     Returns:
         video writer
     """
-    from pathlib import Path
-
-    save_dir = create_dir(output_path)
-    save_path = str(Path(save_dir) / Path(video_path).name)
     if fps is None:
         cap = cv2.VideoCapture(video_path)
         fps = cap.get(cv2.CAP_PROP_FPS)
@@ -91,7 +87,7 @@ def create_video_writer(video_path, output_path, fps=None) -> cv2.VideoWriter:
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     size = (width, height)
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-    videoWriter = cv2.VideoWriter(save_path, fourcc, fps, size)
+    videoWriter = cv2.VideoWriter(output_path, fourcc, fps, size)
     return videoWriter
 
 
